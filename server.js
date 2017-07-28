468aaa5fea4ce728682c477590cd0b176f5f8b9e
Itmarz <804983409@qq.com>
2017-07-28 12:50:00 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config.dev');
-
+var config = require('./webpack.config.dev
 //代理服务器
 var proxy = [{
 	path: '/*/*', //必须得有一个文件地址，如果顶层文件夹名字不同，则用/*代替
